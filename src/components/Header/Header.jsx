01b03f77f9a91afd99c14cf9c4b6082038d6cc50
Giovanni Crudele <crudelegio@gmail.com>
2024-07-03 19:13:11 +0200

    Added active link for categories and data attributes.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../../../backend/data.json";
 
 const Header = ({ filterOnClick }) => {
   // Extract categories from data
   const categories = data.data.categories;
+
+  //state for active links:
+  const [activeLink, setActiveLink] = useState("all");
+
+  //function to handle active links (to filter and update state with active link)
+  const handleCategoryClick = (category) => {
+    setActiveLink(category);
+    filterOnClick(category);
+  };
 
   return (
     <header id="main-header">
@@ -11,21 +20,38 @@ const Header = ({ filterOnClick }) => {
         <img src="logo.jpg" alt="E corp App" />
         <h1>E-Corp App</h1>
       </div>
-      <p>
+      <div className="d-flex justify-content-between">
+        {/* We map categories in a list styled with bootstrap cards */}
         {categories.map((category) => (
-          <button
-            key={category.name}
-            type="button"
-            className="btn btn-primary me-2"
-            onClick={() => filterOnClick(category.name)}
-          >
-            {category.name}
-          </button>
+          <ul className="navbar-nav" key={category.name}>
+            <li className="nav-item">
+              <a
+                type="button"
+                // to filter and update state with active link
+                className={`nav-link ${
+                  activeLink === category.name ? "active" : ""
+                }`}
+                // to select the active link
+                data-testid={
+                  activeLink === category.name
+                    ? "active-category-link"
+                    : "category-link"
+                }
+                onClick={() => handleCategoryClick(category.name)}
+              >
+                {category.name}
+              </a>
+            </li>
+          </ul>
         ))}
-        <button type="button" className="btn btn-warning">
+        <button
+          type="button"
+          className="btn btn-warning"
+          data-testid="cart-btn"
+        >
           Cart
         </button>
-      </p>
+      </div>
     </header>
   );
 };
